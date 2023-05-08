9c0ac93c3eda546215cdf7f6a6cf0ab6fcda85d1
Luan Rodrigues Boschini <cd>
2023-05-08 20:25:37 -0300

    Oi dlc

--- entidade/game.py
+++ entidade/game.py
@@ -1,1 +1,22 @@
 # Luan
+#Como a classe sport não existe foi trocado por um string
+#isTraded foi retirado por não termos mais marketplace
+#Como é xadrez 1x1 foi retirado n_contestant por enquanto e sport
+#Sentiu-se a necessidade de criar uma classe chamada Result
+#Ira analisar se fica melhor implementar o odds como dict ou como uma classe própria
+
+class Game():
+    def __init__(self, id : int, odds : dict, bets : bets, status : bool, aceept_bet : bool, n_contestant : int):
+        if not isinstance(id, int):
+            raise
+        if not isinstance(odds, ):
+            raise
+        if not isinstance(status, bool):
+            raise
+        
+        
+        #Atributos inicializados vazios 
+        self.__pool = float()
+        self.__result = bool()
+        self.__odds = Odds()
+    
---
+++ entidade/odds.py
@@ -0,0 +1,28 @@
+#Luan
+
+import Player from player
+
+class Odds():
+    def __init__(self, player1 : Player, 
+                 player2 : Player, 
+                 bet_vict_player1 : float, 
+                 bet_vict_player2 : float):
+        if not (isinstance( player1, Player)):
+            raise
+        if not (isinstance( player2, Player)):
+            raise
+        self.__player1 = player1.stats()
+        self.__player2 = player2.stats()
+        self.__odd_vict1 = float()
+        self.__odd_vict2 = float()
+        self.__odd_draw = float()
+    
+    @property
+    def player1(self):
+        return self.__player1
+    
+    @property
+    def player2(self):
+        return self.__player2
+    
+    
