72b3ef28de6e3c3f4da72b59bd53b2bfb7976c77
viniguazz <viniguazz@hotmail.com>
2023-05-09 18:42:28 -0300

    kill me, please

--- entidade/bet.py
+++ entidade/bet.py
@@ -24,3 +24,37 @@ class Bet():
     @property
     def price(self):
         return self.__price
+    @property
+    def game(self):
+        return self.__game
+    @property
+    def better(self):
+        return self.__better
+    @property
+    def result(self):
+        return self.__result
+
+    @id.setter
+    def id(self, id):
+        if isinstance(id, int):
+            self.__id = id
+    @status.setter
+    def status(self, status):
+        if isinstance(status, bool):
+            self.__status = status
+    @price.setter
+    def price(self, price):
+        if isinstance(price, int):
+            self.__price = price
+    @game.setter
+    def game(self, game):
+        if isinstance(game, Game):
+            self.__game = game
+    @better.setter
+    def better(self, better):
+        if isinstance(better, Better):
+            self.__better = better
+    @result.setter
+    def result(self, return):
+        if isinstance(result, str):
+            self.__result = result
